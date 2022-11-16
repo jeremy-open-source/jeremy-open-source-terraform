71d9db4c5e8ee7c5ebbd3f10b3158c0e2de0f6ce
Jeremy Sells <mrjeremysells@gmail.com>
2022-11-16 21:08:46 +1300

    Importing existing projects

--- terraform-src/repos.tf
+++ terraform-src/repos.tf
@@ -1,3 +1,30 @@
+module "simple-gitlab-build-agent" {
+  source = "./modules/repo-shared-synced"
+  name = "simple-gitlab-build-agent"
+  description = "Simple Gitlab build agent in docker-compose"
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "esp-lcd-http-message-display" {
+  source = "./modules/repo-shared-synced"
+  name = "esp-lcd-http-message-display"
+  description = "HTTP client for displaying messages on an ESP with an LCD (i.e. ESP82660, HTIT-W8266)"
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "nix-tools" {
+  source = "./modules/repo-shared-synced"
+  name = "nix-tools"
+  description = "My personal scripts for running things"
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
 module "ci-test" {
   source = "./modules/repo-shared-synced"
   name = "ci-test"
