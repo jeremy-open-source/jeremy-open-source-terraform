5b6b4077a81d586d12fad33b0b6f707c1b85d259
Jeremy Sells <mrjeremysells@gmail.com>
2023-06-09 22:11:53 +1200

    Archived old repos

---
+++ terraform-src/repos-archived.tf
@@ -0,0 +1,71 @@
+module "esp-stack-trace-decoder-docker" {
+  archived = true
+  source = "./modules/repo-shared-synced"
+  name = "esp-stack-trace-decoder-docker"
+  description = "Dockerization of https://github.com/littleyoda/EspStackTraceDecoder"
+  pipelines_enabled = true
+  container_registry_enabled = true
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "docker-command-loop-container" {
+  archived = true
+  source = "./modules/repo-shared-synced"
+  name = "docker-command-loop-container"
+  description = "Container to run a command in a loop such as nmap, ping, curl etc"
+  pipelines_enabled = true
+  container_registry_enabled = true
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "ansible-docker" {
+  archived = true
+  source = "./modules/repo-shared-synced"
+  name = "ansible-docker"
+  description = "Custom Ansible Docker Container"
+  pipelines_enabled = true
+  container_registry_enabled = true
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "platformio-docker" {
+  archived = true
+  source = "./modules/repo-shared-synced"
+  name = "platformio-docker"
+  description = "Custom PlatformIO Docker container"
+  pipelines_enabled = true
+  container_registry_enabled = true
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "terraform-docker" {
+  archived = true
+  source = "./modules/repo-shared-synced"
+  name = "terraform-docker"
+  description = "Custom Terraform Docker container"
+  pipelines_enabled = true
+  container_registry_enabled = true
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
+
+module "kubectl-docker" {
+  archived = true
+  source = "./modules/repo-shared-synced"
+  name = "kubectl-docker"
+  description = "kubectl and Doppler in a Docker container"
+  pipelines_enabled = true
+  container_registry_enabled = true
+  gitlab_group_id = gitlab_group.jeremy-open-source.id
+  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
+  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
+}
--- terraform-src/repos.tf
+++ terraform-src/repos.tf
@@ -9,17 +9,6 @@ module "docker-containers" {
   github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
 }
 
-module "docker-command-loop-container" {
-  source = "./modules/repo-shared-synced"
-  name = "docker-command-loop-container"
-  description = "Container to run a command in a loop such as nmap, ping, curl etc"
-  pipelines_enabled = true
-  container_registry_enabled = true
-  gitlab_group_id = gitlab_group.jeremy-open-source.id
-  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
-  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
-}
-
 module "python-docker-cli-cookiecutter" {
   source = "./modules/repo-shared-synced"
   name = "python-docker-cli-cookiecutter"
@@ -75,50 +64,6 @@ module "ci-test" {
   github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
 }
 
-module "ansible-docker" {
-  source = "./modules/repo-shared-synced"
-  name = "ansible-docker"
-  description = "Custom Ansible Docker Container"
-  pipelines_enabled = true
-  container_registry_enabled = true
-  gitlab_group_id = gitlab_group.jeremy-open-source.id
-  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
-  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
-}
-
-module "esp-stack-trace-decoder-docker" {
-  source = "./modules/repo-shared-synced"
-  name = "esp-stack-trace-decoder-docker"
-  description = "Dockerization of https://github.com/littleyoda/EspStackTraceDecoder"
-  pipelines_enabled = true
-  container_registry_enabled = true
-  gitlab_group_id = gitlab_group.jeremy-open-source.id
-  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
-  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
-}
-
-module "platformio-docker" {
-  source = "./modules/repo-shared-synced"
-  name = "platformio-docker"
-  description = "Custom PlatformIO Docker container"
-  pipelines_enabled = true
-  container_registry_enabled = true
-  gitlab_group_id = gitlab_group.jeremy-open-source.id
-  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
-  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
-}
-
-module "terraform-docker" {
-  source = "./modules/repo-shared-synced"
-  name = "terraform-docker"
-  description = "Custom Terraform Docker container"
-  pipelines_enabled = true
-  container_registry_enabled = true
-  gitlab_group_id = gitlab_group.jeremy-open-source.id
-  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
-  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
-}
-
 module "jeremy-open-source-terraform" {
   source = "./modules/repo-shared-synced"
   name = "jeremy-open-source-terraform"
@@ -127,17 +72,6 @@ Jeremy Open Source Terraform
 EOF
   request_access_enabled = false
   pipelines_enabled = true
-  gitlab_group_id = gitlab_group.jeremy-open-source.id
-  github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
-  github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
-}
-
-module "kubectl-docker" {
-  source = "./modules/repo-shared-synced"
-  name = "kubectl-docker"
-  description = "kubectl and Doppler in a Docker container"
-  pipelines_enabled = true
-  container_registry_enabled = true
   gitlab_group_id = gitlab_group.jeremy-open-source.id
   github_sync_in_username = var.GITHUB_SYNC_IN_USERNAME
   github_sync_in_password = var.GITHUB_SYNC_IN_PASSWORD
